e4eabb59aa07018553feb09c7a1a01eb29bf7362
RanielLegaspi <111836115+RanielLegaspi@users.noreply.github.com>
2023-03-20 21:45:19 +0000

    Mon, Mar 20, 2023, 2:45 PM -07:00

--- js/jokes.js
+++ js/jokes.js
@@ -1,4 +1,13 @@
 const jokeEl = document.getElementById ('joke')
 const jokeBtn =  document.getElementById ('jokeBtn')
 
-jokeBtn.addEventListener
+jokeBtn.addEventListener('click', generateJoke)
+
+async function generateJoke(){
+    const response = await fetch ('http://icanhazdadjoke.com', {
+        headers: {Accept: 'application/json'}
+    })
+
+    const data = await response.json()
+    jokeEl.innerHTML = data.joke
+}
