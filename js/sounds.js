f7708028d53be460fd57ce0395b9fff218702d63
RanielLegaspi <111836115+RanielLegaspi@users.noreply.github.com>
2023-03-20 21:24:30 +0000

    Mon, Mar 20, 2023, 2:24 PM -07:00

---
+++ js/sounds.js
@@ -0,0 +1,22 @@
+const sounds = ['applause', 'boo', 'gasp','tada', 'victory', 'wrong' ]
+
+sounds.forEach(sound =>{
+    const btn = document.createElement('button')
+    btn.classList.add('btn')
+    btn.innerText = sound
+
+    btn.addEventListner('click', () => {
+        stopSongs()
+        document.getElementById(sound).play()
+    })
+
+    document.getElementById('buttons').appendChild(btn)
+})
+
+function stopSongs() {
+    sounds.forEach(sounds => {
+        const songs = doncument.getElementById(sound)
+        song.pause()
+        song.currentTime = 0
+    })
+}
