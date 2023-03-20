4e34b9890e2e4991291fbcd3d3a225fe323e5665
RanielLegaspi <111836115+RanielLegaspi@users.noreply.github.com>
2023-03-20 21:30:07 +0000

    Mon, Mar 20, 2023, 2:30 PM -07:00

--- js/sounds.js
+++ js/sounds.js
@@ -1,6 +1,6 @@
 const sounds = ['applause', 'boo', 'gasp','tada', 'victory', 'wrong' ]
 
-sounds.forEach(sound =>{
+sounds.forEach(sound => {
     const btn = document.createElement('button')
     btn.classList.add('btn')
     btn.innerText = sound
