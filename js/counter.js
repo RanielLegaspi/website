a1ec102b1dd9aa4f3a3841ba553048a37ff58d4a
RanielLegaspi <111836115+RanielLegaspi@users.noreply.github.com>
2023-03-21 21:52:19 +0000

    Tue, Mar 21, 2023, 2:52 PM -07:00

--- js/counter.js
+++ js/counter.js
@@ -4,12 +4,13 @@ counters.forEach(counter => {
     counter.innerText = '0'
 
     const updateCounter = () => {
-        const target = +counter.GetAttribute('data-target')
+        const target = +counter.getAttribute('data-target')
         const c = +counter.innerText
         const increment = 1
 
         if (c < target) {
-            counter.innerText = `${Math}`
+            counter.innerText = `${Math.ceil(c + increment)}`
+            setTimeout(updateCounter, 1)
         }
     }
 
