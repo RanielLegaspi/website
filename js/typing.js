24da2c8ed5c98e1f7f4d99bb1c14508a40028e24
RanielLegaspi <111836115+RanielLegaspi@users.noreply.github.com>
2023-03-24 22:03:14 +0000

    Fri, Mar 24, 2023, 3:03 PM -07:00

--- js/typing.js
+++ js/typing.js
@@ -3,13 +3,15 @@ const speedEl = document.getElementById('speed')
 const text = "I am really Smart!!!"
 let idx = 1
 let speed = 275/speedEl.value
+speedEl.addEventListener('input', (element) =>{
+    speed = 275/element.target.value
+})
 
 writeText()
 
 function writeText(){
     textEl.innerText = text.slice(0,idx)
     idx++
-
     if (idx > text.length){
         idx = 1
     }
